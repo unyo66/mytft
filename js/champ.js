9d4f3319cd5e067ff09982e0423c3f2097a139ea
unyo66 <lljjll1235@gmail.com>
2023-10-13 16:53:00 +0900

    initial commit

---
+++ js/champ.js
@@ -0,0 +1,15 @@
+let champ = [{
+    name:"champ1",
+    cost:"1",
+    job1:"job1",
+    job2:"job2",
+    job3:"job3",
+    img:""
+},{
+    name:"champ2",
+    cost:"5",
+    job1:"job5",
+    job2:"job6",
+    job3:"job7",
+    img:""
+}]
